2d9fbc906d98dbaf7df89a6d03e17b1937ea1cc6
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-09-19 01:01:56 +0530

    integrated quill text editor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import "./App.css";
 import TextEditor from "./components/TextEditor";
+import { v4 as uuid } from "uuid";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <TextEditor />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate replace to={`/document/${uuid()}`} />}
+        />
+        <Route path="/document/:id" element={<TextEditor />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,9 +1,43 @@
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { useParams } from "react-router-dom";
+
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
 
 const TextEditor = () => {
   const [text, setText] = useState("");
+  const [quill, setQuill] = useState();
   const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const quillServer = new Quill("#container", {
+      theme: "snow",
+      modules: { toolbar: toolbarOptions },
+    });
+    quillServer.disable();
+    quillServer.setText("Loading the document...");
+    setQuill(quillServer);
+  }, []);
 
   useEffect(() => {
     // Connect to the Socket.io server (replace 'server-url' with your server's URL)
@@ -28,13 +62,18 @@ const TextEditor = () => {
   };
 
   return (
-    <div>
-      <textarea
-        value={text}
-        onChange={(e) => handleTextChange(e.target.value)}
-        rows={10}
-        cols={40}
-      />
+    <div className="bg-gray-100 h-screen p-4">
+      <div className="bg-white p-4 rounded shadow-md">
+        <h1 className="text-2xl font-semibold mb-4">Real-Time Text Editor</h1>
+        <textarea
+          id="container"
+          className="w-full p-2 border rounded-md resize-none"
+          value={text}
+          onChange={(e) => handleTextChange(e.target.value)}
+          rows={10}
+          placeholder="Start typing here..."
+        />
+      </div>
     </div>
   );
 };
